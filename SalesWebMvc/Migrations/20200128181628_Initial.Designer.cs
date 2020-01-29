c2cf81b65367cc7ec0966b253b1b6ea617c69ff4
valerio assis <lelo_asis@hotmail.com>
2020-01-29 09:33:57 -0300

    Other Entities and second migration.

--- SalesWebMvc/Migrations/20200128181628_Initial.Designer.cs
+++ SalesWebMvc/Migrations/20200128181628_Initial.Designer.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using SalesWebMvc.Data;
+using SalesWebMvcModels;
 
 namespace SalesWebMvc.Migrations
 {
